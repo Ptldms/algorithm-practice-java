ba10d453a8a74d9ee14287c27e4ed465569d3ae7
Ptldms <ptldms56@gmail.com>
2026-03-19 17:51:25 +0900

    [ADD] 1장 기본 알고리즘
    01-1. 알고리즘이란? (p.13~25)

---
+++ src/chapter01/exercise/Q3.java
@@ -0,0 +1,15 @@
+package chapter01.exercise;
+
+public class Q3 {
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        if (d < min) min = d;
+
+        return min;
+    }
+    public static void main(String[] args) {
+        System.out.println(min4(2, 4, 3, 1));
+    }
+}
