3929bfde73719237258dc143de07d2f57328a526
Ptldms <ptldms56@gmail.com>
2026-04-08 21:10:18 +0900

    [ADD] 3장 검색 알고리즘
    03-2. 선형 검색 (p.101 ~ 105)

--- src/chapter03/practice/practice3_3.java
+++ src/chapter03/practice/practice3_3.java
@@ -2,37 +2,28 @@ package chapter03.practice;
 
 import java.util.Scanner;
 
-public class practice3_1 {
-    // while 문
-    static int seqSearch(int[] a, int n, int key) {
+public class practice3_3 {
+
+    static int seqSearchSen(int[] a, int n, int key) {
         int i = 0;
 
-        while(true) {
-            if (i == n)
-                return -1;
-            if(a[i] == key)
-                return i;
+        a[n] = key;
+
+        while (true) {
+            if (a[i] == key)
+                break;
             i++;
         }
-    }
 
-    /*
-    // for 문
-    static int seqSearch(int[] a, int n, int key) {
-        for (int i = 0; i < n; i++)
-            if (a[i] == key)
-                return i;
-
-        return -1;
+        return i == n ? -1 : i;
     }
-    */
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
         System.out.print("요솟수: ");
         int num = stdIn.nextInt();
-        int[] x = new int[num];
+        int[] x = new int[num + 1];
 
         for (int i = 0; i < num; i++) {
             System.out.print("x[" + i + "]: ");
@@ -41,7 +32,7 @@ public class practice3_1 {
 
         System.out.print("검색할 값: ");
         int ky = stdIn.nextInt();
-        int idx = seqSearch(x, num, ky);
+        int idx = seqSearchSen(x, num, ky);
 
         if (idx == -1)
             System.out.println("그 값의 요소가 없습니다.");
