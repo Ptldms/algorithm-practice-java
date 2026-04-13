6c5ef3a7b1551d3cd1d7c77fc742e914bf7473b7
Ptldms <ptldms56@gmail.com>
2026-04-13 17:27:11 +0900

    [ADD] 3장 검색 알고리즘
    03-3. 이진 검색 (p.115)

---
+++ src/chapter03/exercise/Q5.java
@@ -0,0 +1,67 @@
+package chapter03.exercise;
+
+import java.util.Scanner;
+
+public class Q5 {
+
+    static int binSearchX(int[] a, int n, int key) {
+        int pl = 0;
+        int pr = n - 1;
+
+        do {
+            int pc = (pl + pr) / 2;
+
+            if (a[pc] == key) {
+                int front = pc;
+                while (a[front] == key)
+                    front--;
+                return (front + 1);
+
+                /*
+                // 정답
+                for ( ; pc > pl; pc--)
+                    if (a[pc - 1] < key)
+                        break;
+                return pc;
+                */
+            }
+            else if (a[pc] < key)
+                pl = pc + 1;
+            else
+                pr = pc - 1;
+        } while (pl <= pr);
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int num = stdIn.nextInt();
+        int[] x = new int[num];
+
+        System.out.println("오름차순으로 입력하세요.");
+
+        System.out.print("x[0]: ");
+        x[0] = stdIn.nextInt();
+
+        for (int i = 1; i < num; i++) {
+            do {
+                System.out.print("x[" + i + "]: ");
+                x[i] = stdIn.nextInt();
+            } while (x[i] < x[i - 1]);
+        }
+
+        System.out.print("검색할 값: ");
+        int ky = stdIn.nextInt();
+
+        int idx = binSearchX(x, num, ky);
+
+        if (idx == 1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println("그 값과 일치하는 맨 앞의 요소는 x[" + idx + "]에 있습니다.");
+    }
+
+}
